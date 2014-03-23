bed4f676aeb0435e247de4f0189bde0e582a3590
Michael J <michael.git@shadowfox.net>
2014-03-23 21:55:41 +1000

    Hook up twig for templating. Set up an app environment constant and add another constant for the location of the app directory itself

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+define('APP_ENV', !empty($_SERVER['APP_ENV']) ? $_SERVER['APP_ENV'] : 'production');
+define('APP_PATH', ROOT_PATH . '/app');
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 // Components to load
@@ -25,12 +28,31 @@ function getKleinRequest() {
 	return $request;
 }
 
+$router->respond(function ($request, $response, $service, $app) use ($router) {
+	// Hook up twig
+	$app->register('twig', function() {
+		$loader = new Twig_Loader_Filesystem(APP_PATH . '/templates/');
+		$twig = new Twig_Environment($loader, [
+			'cache' => ROOT_PATH . '/cache/',
+			'strict_variables' => true
+		]);
+
+		if (APP_ENV === 'development') {
+			$twig->enableDebug();
+			$twig->enableAutoReload();
+			$twig->addExtension(new Twig_Extension_Debug());
+		}
+
+		return $twig;
+	});
+});
+
 // Load the controllers
 foreach ($controllers as $controller) {
-	require_once ROOT_PATH . '/app/controllers/' . $controller . '.php';
+	require_once APP_PATH . '/controllers/' . $controller . '.php';
 }
 
 // Load the models
 foreach ($models as $model) {
-	require_once ROOT_PATH . '/app/models/' . $model . '.php';
+	require_once APP_PATH . '/models/' . $model . '.php';
 }
