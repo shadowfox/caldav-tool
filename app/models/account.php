2837e2b4dd42b1e033fcaaa038d2a0f65da3c4e1
Michael J <michael.git@shadowfox.net>
2014-03-29 21:47:35 +1000

    Add method to account model which attempts to guess the who hosts the caldav server (apple, google, etc.) Fixed indentation to spaces for both model files

--- app/models/account.php
+++ app/models/account.php
@@ -3,16 +3,26 @@
  * Represents a CalDAV account which can have one or many calendars
  */
 class Account extends Model {
-	public function calendars() {
-		return $this->hasMany('Calendar');
-	}
+    public function calendars() {
+        return $this->hasMany('Calendar');
+    }
 
-	public function getBasicAuthString() {
-		return base64_encode($this->username . ':' . $this->password);
-	}
+    public function serverType() {
+        if (strpos($this->server_uri, 'icloud') !== false) {
+            return 'icloud';
+        } else if (strpos($this->server_uri, 'google') !== false) {
+            return 'google';
+        } else {
+            return 'generic';
+        }
+    }
 
-	public function __toString() {
-		return sprintf('Account[id=%s, server=%s, uri=%s, username=%s]',
-			$this->id, $this->server, $this->uri, $this->username);
-	}
+    public function getBasicAuthString() {
+        return base64_encode($this->username . ':' . $this->password);
+    }
+
+    public function __toString() {
+        return sprintf('Account[id=%s, server=%s, uri=%s, username=%s]',
+            $this->id, $this->server, $this->uri, $this->username);
+    }
 }
--- app/models/calendar.php
+++ app/models/calendar.php
@@ -3,12 +3,12 @@
  * Represents access information for a CalDAV calendar
  */
 class Calendar extends Model {
-	public function account() {
-		return $this->hasOne('Account');
-	}
+    public function account() {
+        return $this->hasOne('Account');
+    }
 
-	public function __toString() {
-		return sprintf('Calendar[id=%s, account_id=%s, name=%s, uri=%s]',
-			$this->id, $this->account_id, $this->name, $this->uri);
-	}
+    public function __toString() {
+        return sprintf('Calendar[id=%s, account_id=%s, name=%s, uri=%s]',
+            $this->id, $this->account_id, $this->name, $this->uri);
+    }
 }
