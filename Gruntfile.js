62eb8523f3f12735c22c46c1d8865d7700b16b43
Michael J <michael.git@shadowfox.net>
2014-03-31 18:10:20 +1000

    Add the application specific css and javascript assets. Overhaul gruntfile to do css and javascript minifying along with automatic task running

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,29 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      concat: {
-        js: {
-          src: [
-            './bower_components/jquery/dist/jquery.min.js',
-            './bower_components/bootstrap/dist/js/bootstrap.min.js'
-          ],
-          dest: './web/js/app.js'
+      cssmin: {
+        combine: {
+          files: {
+            './web/css/style.min.css': [
+              './bower_components/bootstrap/dist/css/bootstrap.min.css',
+              './assets/css/overrides.css',
+              './assets/css/app.css'
+            ]
+          }
+        }
+      },
+      uglify: {
+        options: {
+          sourceMap: true,
         },
-        css: {
-          src: [
-            './bower_components/bootstrap/dist/css/bootstrap.min.css',
-          ],
-          dest: './web/css/style.css'
+        main: {
+          files: {
+            './web/js/app.min.js': [
+              './bower_components/jquery/dist/jquery.min.js',
+              './bower_components/bootstrap/dist/js/bootstrap.min.js',
+              './assets/js/*.js'
+            ]
+          }
         }
       },
       copy: {
@@ -27,16 +37,41 @@ module.exports = function(grunt) {
           src: ['**'],
           dest: './web/fonts/'
         }
+      },
+      watch: {
+        css: {
+          files: './assets/css/*.css',
+          tasks: ['cssmin'],
+          options: {
+            debounceDelay: 250
+          }
+        },
+        js: {
+          files: './assets/js/*.js',
+          tasks: ['uglify'],
+          options: {
+            debounceDelay: 250
+          }
+        },
+        // Reload the watcher when this config file changes
+        grunt: {
+          files: ['Gruntfile.js'],
+          options: {
+            reload: true
+          }
+        }
       }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // Load all required tasks
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'copy']);
+  grunt.registerTask('default', ['cssmin', 'uglify', 'copy']);
 
+  // Runs tasks when files change
+  grunt.registerTask('dev', ['watch']);
 };
