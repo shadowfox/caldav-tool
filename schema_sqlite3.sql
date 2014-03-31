962ee656b51f74dbbef04ae1caad2e585631918a
Michael J <michael.git@shadowfox.net>
2014-03-31 18:40:58 +1000

    Minor change to schema to enforce not-nulls when required

--- schema_sqlite3.sql
+++ schema_sqlite3.sql
@@ -1,13 +1,13 @@
 CREATE TABLE account (
 	id TEXT PRIMARY KEY,
-	server_uri TEXT,
+	server_uri TEXT NOT NULL,
 	username TEXT,
 	password TEXT
 );
 
 CREATE TABLE calendar (
 	id TEXT PRIMARY KEY,
-	account_id TEXT,
-	name TEXT,
-	uri TEXT
+	account_id TEXT NOT NULL,
+	name TEXT NOT NULL,
+	uri TEXT NOT NULL
 )
